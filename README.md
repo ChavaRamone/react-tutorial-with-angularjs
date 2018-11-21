70f56328b084f252d8ac49ecfe2e2371f38fafdb
Salvador Espinosa Tabaco <salvador.espinosa@globant.com>
2018-11-21 10:00:44 -0600

    Starter code

---
+++ README.md
@@ -0,0 +1,18 @@
+# react-tutorial-with-angularjs
+
+Tic Tac Toe [tutorial](https://reactjs.org/tutorial/tutorial.html) from React made in AngularJS.
+
+### Installation
+
+It just requires [Node.js](https://nodejs.org/) to run.
+
+Install the dependencies and start the server.
+
+```sh
+$ npm install
+$ npm start
+```
+
+License
+----
+MIT
---
+++ app.js
@@ -0,0 +1,77 @@
+function squareController() {
+
+}
+
+function squareTemplate($element, $attrs) {
+  return `
+    <button class="square">
+      <!-- TODO -->
+    </button>`
+}
+
+function boardController() {
+  this.status = 'Next player: X';
+}
+
+function boardTemplate($element, $attrs) {
+  // access to $element and $attrs
+
+  function renderSquare(i) {
+    return `<square></square>`
+  }
+
+  return `
+    <div>
+      <div class="status">{{board.status}}</div>
+      <div class="board-row">
+        ${renderSquare(0)}
+        ${renderSquare(1)}
+        ${renderSquare(2)}
+      </div>
+      <div class="board-row">
+        ${renderSquare(3)}
+        ${renderSquare(4)}
+        ${renderSquare(5)}
+      </div>
+      <div class="board-row">
+        ${renderSquare(6)}
+        ${renderSquare(7)}
+        ${renderSquare(8)}
+      </div>
+    </div>`
+}
+
+function gameController() {
+
+}
+
+function gameTemplate($element, $attrs) {
+  // access to $element and $attrs
+  return `
+    <div class="game">
+      <div class="game-board">
+        <board />
+      </div>
+      <div class="game-info">
+        <div><!-- status --></div>
+        <ol><!-- TODO --></ol>
+      </div>
+    </div>`
+}
+
+angular.module('ticTacToe', [])
+  .component('square', {
+    controller: squareController,
+    controllerAs: 'square',
+    template: squareTemplate,
+  })
+  .component('board', {
+    controller: boardController,
+    controllerAs: 'board',
+    template: boardTemplate,
+  })
+  .component('game', {
+    controller: gameController,
+    controllerAs: 'game',
+    template: gameTemplate,
+  })

da00c7702ea3194edb7b33de325372e5e0ad1b35
Salvador Espinosa Tabaco <salvador.espinosa@globant.com>
2018-11-22 10:17:55 -0600

    Declaring a Winner

--- app.js
+++ app.js
@@ -15,10 +15,18 @@ function boardController() {
   this.xIsNext = true;
   this.handleClick = function (i) {
     const squares = this.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.xIsNext ? 'X' : 'O';
     this.squares = squares;
     this.xIsNext = !this.xIsNext;
-    this.status = `Next player: ${this.xIsNext ? 'X' : 'O'}`
+    const winner = calculateWinner(this.squares);
+    if (winner) {
+      this.status = 'Winner: ' + winner;
+    } else {
+      this.status = `Next player: ${this.xIsNext ? 'X' : 'O'}`;
+    }
   }
 }
 
@@ -66,6 +74,26 @@ function gameTemplate($element, $attrs) {
         <ol><!-- TODO --></ol>
       </div>
     </div>`
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 angular.module('ticTacToe', [])
